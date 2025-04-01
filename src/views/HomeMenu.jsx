be160161cccf7690b99e0504c85a6ab75fb35d07
thidarat008 <s6419410008@sau.ac.th>
2025-04-01 10:58:26 +0700

    v.2แก้หน้า homemenu

--- src/views/HomeMenu.jsx
+++ src/views/HomeMenu.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Box,Typography, Button, Avatar } from "@mui/material";
 
 function HomeMenu() {
-  return <>Thidarat Hirun</>;
+  return (
+    <>
+      <Box sx={{height: '100vh', display: 'flex'  }}>
+      <Box Sx={{width: '70%',boxShadow:  2, mx: 'auto', my: 'auto', p:  3}}>
+        <Avatar src={'https://www.w3schools.com/howto/img_avatar.png'}
+        sx={{width: 150, height: 150, mx: 'auto'}}/>
+        <Typography variant='h3' sx={{ textAlign: 'center' , mt: 2}}>
+          IoT Calculate by thidarat
+        </Typography>
+      </Box>
+      </Box>
+    </>
+  )
 }
 
 export default HomeMenu;
