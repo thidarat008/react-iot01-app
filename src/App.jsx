b6c80654276a3c14a12f4ca41bf807d4b21459e1
thidarat008 <s6419410008@sau.ac.th>
2025-04-01 11:08:07 +0700

    แก้

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { CssBaseline } from '@mui/material'
-import { Calculate } from '@mui/icons-material'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import { Calculate } from "@mui/icons-material";
 
-import HomeMeun from './views/HomeMenu'
-import CalculateNumber from './views/CalculateNumber'
-import CalculateMoneyShare from './views/CalculateMoneyShare'
-
+import HomeMeun from "./views/HomeMenu";
+import CalculateNumber from "./views/CaculateNumber";
+import CalculateMoneyShare from "./views/CaculateMoneyShare";
 
 function App() {
   return (
     <>
-    {/* ใช้ เพื่อ รูปแบบ ทั้งหมด */} 
-      <Calculate /> 
+      {/* ใช้ เพื่อ รูปแบบ ทั้งหมด */}
       <CssBaseline />
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeMeun />} />
-        <Route path="/calnum" element={<CalculateNumber />} />
-        <Route path="/calmoneyshare" element={<CalculateMoneyShare />} />
+        <Routes>
+          <Route path="/" element={<HomeMeun />} />
+          <Route path="/calnum" element={<CalculateNumber />} />
+          <Route path="/calmoneyshare" element={<CalculateMoneyShare />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/views/HomeMenu.jsx
+++ src/views/HomeMenu.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { Box,Typography, Button, Avatar } from "@mui/material";
+import { Box, Typography, Button, Avatar } from "@mui/material";
 
 function HomeMenu() {
   return (
     <>
-      <Box sx={{height: '100vh', display: 'flex'  }}>
-      <Box Sx={{width: '70%',boxShadow:  2, mx: 'auto', my: 'auto', p:  3}}>
-        <Avatar src={'https://www.w3schools.com/howto/img_avatar.png'}
-        sx={{width: 150, height: 150, mx: 'auto'}}/>
-        <Typography variant='h3' sx={{ textAlign: 'center' , mt: 2}}>
-          IoT Calculate by thidarat
-        </Typography>
-      </Box>
+      <Box sx={{ height: "100vh", display: "flex" }}>
+        <Box sx={{ width: "70%", boxShadow: 2, mx: "auto", my: "auto", p: 3 }}>
+          <Avatar
+            src={"https://www.w3schools.com/howto/img_avatar.png"}
+            sx={{ width: 150, height: 150, mx: "auto" }}
+          />
+          <Typography variant="h3" sx={{ textAlign: "center", mt: 2 }}>
+            IoT Calculate by thidarat
+          </Typography>
+        </Box>
       </Box>
     </>
-  )
+  );
 }
 
 export default HomeMenu;
