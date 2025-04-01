9a7a019a8049aca5c73064c42ac2d370fc5f5658
thidarat008 <s6419410008@sau.ac.th>
2025-04-01 11:16:28 +0700

    แก้รูปครั้งที่ 100

--- src/views/HomeMenu.jsx
+++ src/views/HomeMenu.jsx
@@ -7,7 +7,7 @@ function HomeMenu() {
       <Box sx={{ height: "100vh", display: "flex" }}>
         <Box sx={{ width: "70%", boxShadow: 2, mx: "auto", my: "auto", p: 3 }}>
           <Avatar
-            src={"https://www.w3schools.com/howto/img_avatar.png"}
+            src={"https://cdn.pixabay.com/photo/2024/08/14/11/26/posy-8968312_1280.jpg"}
             sx={{ width: 150, height: 150, mx: "auto" }}
           />
           <Typography variant="h3" sx={{ textAlign: "center", mt: 2 }}>
