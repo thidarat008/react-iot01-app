bbedc2919b82aa9c456f81dd3c3fe8da5272510e
thidarat008 <s6419410008@sau.ac.th>
2025-04-01 11:13:36 +0700

    แก้2

--- src/views/HomeMenu.jsx
+++ src/views/HomeMenu.jsx
@@ -5,9 +5,9 @@ function HomeMenu() {
   return (
     <>
       <Box sx={{ height: "100vh", display: "flex" }}>
-        <Box x={{ width: "70%", boxShadow: 2, mx: "auto", my: "auto", p: 3 }}>
+        <Box sx={{ width: "70%", boxShadow: 2, mx: "auto", my: "auto", p: 3 }}>
           <Avatar
-            src={"https://cdn.pixabay.com/photo/2024/08/14/11/26/posy-8968312_1280.jpg"}
+            src={"https://www.w3schools.com/howto/img_avatar.png"}
             sx={{ width: 150, height: 150, mx: "auto" }}
           />
           <Typography variant="h3" sx={{ textAlign: "center", mt: 2 }}>
